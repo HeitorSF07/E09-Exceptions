c877c1ae7a85bea2a195ea294c4a5fe3efa8f317
Heitor de Sousa Freitas <heitorsf2007@gmail.com>
2024-09-02 16:38:31 -0300

    Update ContaCorrente.java

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,12 +1,11 @@
 public class ContaCorrente extends Conta implements ITaxas{
     public ContaCorrente(int numero, Cliente dono, double saldo, double limite) {
+
         super(numero, dono, saldo, limite);
     }
     public boolean setLimite(double limite){
         if (limite < -100) {
-            this.limite = 0;
-            System.out.println("Limite inválido para conta corrente");
-            return false;
+            throw new IllegalArgumentException("Limite inválido para conta corrente");
         } else {
             this.limite = limite;
             return true;
