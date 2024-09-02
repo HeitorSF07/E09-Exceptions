d1b18b6d0e5842010a3eb98fa431e935212cc86f
Heitor de Sousa Freitas <heitorsf2007@gmail.com>
2024-09-02 16:39:49 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,25 @@ public class Main {
         PessoaFisica F1 = new PessoaFisica("João", "Av. Antônio Carlos", "112.345.708-22", 35, 'm');
         Conta contaF1 = new ContaCorrente(1234, F1, 5000, 2000);
 
-        contaF1.sacar(500);
-        contaF1.depositar(2000);
-        contaF1.sacar(1000);
-        contaF1.depositar(2000);
+        try {
+            contaF1.depositar(-2000);
+        }catch(ValorNegativoException i){
+            System.out.println(i.getMessage());
+        }
 
+        try{
+            contaF1.sacar(-200);
+        }catch (ValorNegativoException | SemLimiteException i){
+            System.out.println(i.getMessage());
+        }
 
-        contaF1.imprimirExtrato(1);
-        contaF1.imprimirExtrato(2);
-        contaF1.imprimirExtrato(0);
-        contaF1.imprimirExtratoTaxas();
+        try{
+            contaF1.sacar(2001);
+        }catch (ValorNegativoException | SemLimiteException i){
+            System.out.println(i.getMessage());
+        }
+
+        contaF1.setLimite(-106);
 
     }
 }
